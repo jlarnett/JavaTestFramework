58f9e41813f5c6ce27dace9b34ea132564d296d7
CoyoteGen2 <im10g@hotmail.com>
2025-11-22 10:08:50 -0600

    Adding more steps + features

--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -2,11 +2,18 @@ package driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class DriverFactory {
 
-    private static final ThreadLocal<WebDriver> driver =
-            ThreadLocal.withInitial(EdgeDriver::new);
+    private static final ThreadLocal<WebDriver> driver = ThreadLocal.withInitial(() -> {
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        return new EdgeDriver(options);
+    });
 
     public static WebDriver getDriver() {
         return driver.get();
