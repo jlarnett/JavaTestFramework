3366c8a9dcacba8893ccfd83723440fbe288dcb6
CoyoteGen2 <im10g@hotmail.com>
2025-11-23 15:53:41 -0600

    Added more logic for creating scenarios / cucumber test

--- src/test/java/pages/shared/NavigationBar.java
+++ src/test/java/pages/shared/NavigationBar.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.LoginPage;
 
 import java.time.Duration;
@@ -136,6 +138,8 @@ public class NavigationBar {
      */
     public void checkForProfilePicture(boolean shouldBeVisible) {
         if(shouldBeVisible) {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.visibilityOf(profilePicture));
             final boolean isProfilePictureDisplayed = profilePicture.isDisplayed();
             Assertions.assertTrue(isProfilePictureDisplayed);
         }
